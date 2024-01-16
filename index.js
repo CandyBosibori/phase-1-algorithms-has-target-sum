2d9e7dec2fa387e60ad9703e93dcd322eb26ce1f
Candy <candy.ongaro@student.moringaschool.com>
2024-01-16 23:10:10 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
+// Runtime: O(n)
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+// Runtime: O(n^2)
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     const complement = target - array[i];
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] === complement) return true;
+//     }
+//   }
 
-/* 
-  Add your pseudocode here
-*/
+//   return false;
+// }
 
-/*
-  Add written explanation of your solution here
-*/
+// O(n) runtime
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
+}
+
+// O(1) runtime
+function findSock(object) {
+  if (object.sock) return "sock";
+}
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -29,6 +52,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 2, 7, 4], 6));
 }
 
 module.exports = hasTargetSum;
